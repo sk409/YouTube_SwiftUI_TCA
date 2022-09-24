25bd6528fc2fa258eb1fd1f9696c721d9b2fe236
sk409 <deepstrap353@gmail.com>
2022-09-25 03:23:34 +0900

    init

---
+++ Sources/YouTube/YouTube.swift
@@ -0,0 +1,6 @@
+public struct YouTube {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/YouTubeTests/YouTubeTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import YouTube
+
+final class YouTubeTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(YouTube().text, "Hello, World!")
+    }
+}
