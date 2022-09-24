75d85b37a3350e3df2c10e77ff807a4ba96b8a14
sk409 <deepstrap353@gmail.com>
2022-09-25 03:12:02 +0900

    Initial Commit

---
+++ YouTube/YouTubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouTubeApp.swift
+//  YouTube
+//
+//  Created by 小林聖人 on 2022/09/25.
+//
+
+import SwiftUI
+
+@main
+struct YouTubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
